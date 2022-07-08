980556a14b38f8f6ce10a16787d15e96a3424296
Mario Campbell <mario@cliengo.com>
2022-07-08 01:31:18 -0300

    images import loaders

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 import './style.css';
+import logo from './img/logo.png';
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello World!');
+
+  const app = document.querySelector('#app');
+
+  app.innerHTML = `<img src=${logo} >`;
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"),
-  MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/app.js',
@@ -9,29 +10,42 @@ module.exports = {
         test: /\.js$/i,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.html$/i,
         use: [
           {
-            loader: "html-loader",
+            loader: 'html-loader',
             options: { minimize: true },
           },
         ],
       },
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'img/[name].[ext]',
+            },
+          },
+          'image-webpack-loader?bypassOnDebug',
+        ],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
+      template: './src/index.html',
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin(),
+    new Dotenv(),
   ],
 };
